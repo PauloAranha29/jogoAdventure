d8433f4c76b90317d2202bc2e3e4888fc8fd00dd
Unknown <henrique.braz@acad.pucrs.br>
2018-06-27 20:56:20 -0300

    Criada sala HallEntrada e SalaEsquerda

--- Adventure/src/adventure/Salas/HallEntrada.java
+++ Adventure/src/adventure/Salas/HallEntrada.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Adventure.src.adventure.Salas;
+package adventure.Salas;
 
 import Adventure.src.adventure.ClassesBasicas.Sala;
 import Adventure.src.adventure.Ferramentas.Lanterna;
@@ -27,18 +27,13 @@ public class HallEntrada extends Sala {
     }
     @Override
     public String textoDescricao(){
-        StringBuilder descricao = new StringBuilder();
-        descricao.append("Você, capitão mor da guarda real do planeta LongisPraKarambis, foi escolhido por suas\n " +
-                "habilidades especiais e destacada inteligência para o resgate da princesa Isthar, que foi raptada pelo\n" +
-                " vilão Malus, o Terrível. Após semanas de investigações, seus homens descobrem\n " +
-                "que a princesa está presa no asteroide principal do quadrante sul da galáxia,\n " +
-                "o temível DalhiNinguemScapus. Poderá você resgatar a princesa desta terrível armadilha?\n\n"+
-                "Você finalmente chega ao DalhiNinguemScapus, que seria um asteróide como qualquer outro se não se\n " +
-                "destacasse pelo tamanho e por sua aparência sinistra, parecendo formar a face de uma caveira\n " +
-                "em um dos lados. Você pousa sua nave pelo lado norte, sabendo que ali se encontra uma estreita\n" +
-                " passagem para a sala principal. Munido apenas de sua pistola laser,\n " +
-                "uma lanterna e muita coragem, você abre a cabine de sua nave e se encontra em frente a duas portas\n, " +
-                "uma a direita e outra a esquerda. O que você faz?");
+                 StringBuilder descricao = new StringBuilder();
+		descricao.append("Voce esta no ").append(this.getNome()).append("\n");
+		descricao.append("Parece uma sala completamente vazia. Há duas portas, uma a direita e outra a esquerda\n");
+                descricao.append("Qual porta você escolhe?\n");
+		descricao.append("Objetos: ").append(this.objetosDisponiveis().toString()).append("\n");
+		descricao.append("Ferramentas: ").append(this.ferramentasDisponiveis().toString()).append("\n");
+		descricao.append("Portas: ").append(this.portasDisponiveis().toString()).append("\n");
         return  descricao.toString();
     }
 
@@ -46,11 +41,6 @@ public class HallEntrada extends Sala {
     public boolean usa(String ferramenta) {
         return false;
     }
-    Scanner in = new Scanner(System.in);
-    
-@Override
-    public Sala sai (String sala){
-        Sala aux = super.sai(sala);
+        Scanner in = new Scanner(System.in);
+}    
 
-    }
-}
--- Adventure/src/adventure/Salas/SalaEsquerda.java
+++ Adventure/src/adventure/Salas/SalaEsquerda.java
@@ -3,27 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Adventure.src.adventure.Salas;
+package adventure.Salas;
 
+import Adventure.src.adventure.ClassesBasicas.Ferramenta;
 import Adventure.src.adventure.ClassesBasicas.Sala;
+import Adventure.src.adventure.Ferramentas.JogoChaves;
+import Adventure.src.adventure.Ferramentas.Lanterna;
+import Adventure.src.adventure.Ferramentas.PistolaLaser;
+import java.util.Scanner;
 
 /**
  *
  * @author Henrique
  */
 public class SalaEsquerda extends Sala {
-
+   private boolean escuro;
+   int cont = 0;
     public SalaEsquerda() {
         super("SalaEsquerda");
+          escuro = true;
+          JogoChaves jogoChaves = new JogoChaves();
+	  this.getFerramentas().put(jogoChaves.getDescricao(), jogoChaves);
+        
     }
 
     @Override
     public String textoDescricao() {
-        return null;
+        	StringBuilder descricao = new StringBuilder();
+		descricao.append("Voce esta na ").append(this.getNome()).append("\n");
+		if (escuro) { cont++;
+			descricao.append("Esta escuro aqui e você não consegue ver nada");
+                        
+		}else if(!escuro) {
+		    descricao.append("você verifica um teclado numérico, e uma porta mais a frente.\n");
+                    descricao.append("Não há nada que você possa fazer aqui.\n");
+		    descricao.append("Objetos: ").append(this.objetosDisponiveis().toString()).append("\n");
+		    descricao.append("Ferramentas: ").append(this.ferramentasDisponiveis().toString()).append("\n");
+                    
+		}else if(escuro && cont > 0);{
+                 descricao.append("Você achou um jogoChaves!\n");}
+                
+                descricao.append("Portas: ").append(this.portasDisponiveis().toString()).append("\n");
+		return descricao.toString();
     }
 
-    @Override
+   @Override
+	public boolean pega(String nomeFerramenta) {
+		boolean ok = super.pega(nomeFerramenta);
+		if (ok) {
+		    this.getFerramentas().remove(nomeFerramenta);
+		    return true;
+		}
+		return false;
+	}
+            @Override
     public boolean usa(String ferramenta) {
         return false;
     }
+        Scanner in = new Scanner(System.in);
+        
 }
